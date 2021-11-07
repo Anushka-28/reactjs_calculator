f1e053580960b2cbc444d4e6ab3150b822d7e66b
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-11-07 15:31:55 +0530

    Added buttons of calculator.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+
+        <div className="keypad">
+          <button onClick={clear} id="clear" className="highlight">AC</button>
+          <button onClick={handleClick} name="/" className="highlight">&divide;</button>
+          <button onClick={handleClick} name="*" className="highlight">&times;</button>
+          <button onClick={backspace} id="backspace" className="highlight">C</button>
+          
+          <button onClick={handleClick} name="7">7</button>
+          <button onClick={handleClick} name="8">8</button>
+          <button onClick={handleClick} name="9">9</button>
+          <button onClick={handleClick} name="+" className="highlight">+</button>
+          
+          <button onClick={handleClick} name="4">4</button>
+          <button onClick={handleClick} name="5">5</button>
+          <button onClick={handleClick} name="6">6</button>
+          <button onClick={handleClick} name="-" className="highlight">&ndash;</button>
+
+          <button onClick={handleClick} name="1">1</button>
+          <button onClick={handleClick} name="2">2</button>
+          <button onClick={handleClick} name="3">3</button>
+          <button onClick={handleClick} name="%">%</button>
+          <button onClick={handleClick} name="0">0</button>
+          <button onClick={handleClick} name=".">.</button>
+          <button onClick={calculate} id="result" className="highlight-more">=</button>
+        </div>
+      </div>
+    </>
   );
 }
 
